a0aca4d30fe0d74750d2f44e64b316978cf47524
akdl911215 <akdl911215@naver.com>
2023-10-01 23:46:33 +0900

    spring boot setting and study

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class controller {
+public class SampleController {
 
     @GetMapping("/hello")
     public String[] hello() {
